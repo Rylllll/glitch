95d00289054cfa9a1c9052fb85dbab57a7885bc3
reymark <125524406+Rylllll@users.noreply.github.com>
2026-05-20 15:20:06 +0800

    Fix typo in conditional rendering for visit button

--- src/app/components/WorkOverview.tsx
+++ src/app/components/WorkOverview.tsx
@@ -325,7 +325,7 @@ export function WorkOverview() {
           />
           
           {/* CONDITIONALLY RENDER VISIT BUTTON */}
-          {work.slug !== "pokedex-cms" && work.slug !== "yamaha-motors-cms" && (
+          {work.slug !== "pokedex-cms" && work.slug !== "yamaha-motor-cms" && (
             <a 
               href={work.link}
               target="_blank" 
